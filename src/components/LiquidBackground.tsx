6d1cc0eb53d313f924b59bdd15b3b3c722d1f4b4
Aniket Dubey <aniketdubey1204@gmail.com>
2026-02-06 19:56:16 +0530

    fix: restore black tabs with subtle glass border and enhance blobs

--- src/components/LiquidBackground.tsx
+++ src/components/LiquidBackground.tsx
@@ -7,16 +7,16 @@ export default function LiquidBackground() {
       <div className="absolute inset-0 bg-gradient-to-br from-purple-900/30 via-transparent to-blue-900/30" />
 
       {/* Top Left - Intense Purple */}
-      <div className="absolute top-[-10%] left-[-10%] w-[50vw] h-[50vw] bg-purple-500 rounded-full blur-[100px] opacity-50 animate-blob" />
+      <div className="absolute top-[-10%] left-[-10%] w-[60vw] h-[60vw] bg-purple-500 rounded-full blur-[100px] opacity-60 animate-blob" />
       
       {/* Top Right - Intense Cyan */}
-      <div className="absolute top-[0%] right-[-10%] w-[50vw] h-[50vw] bg-cyan-400 rounded-full blur-[100px] opacity-40 animate-blob animation-delay-2000" />
+      <div className="absolute top-[0%] right-[-10%] w-[60vw] h-[60vw] bg-cyan-400 rounded-full blur-[100px] opacity-50 animate-blob animation-delay-2000" />
       
       {/* Bottom - Intense Pink/Rose */}
-      <div className="absolute bottom-[-10%] left-[20%] w-[60vw] h-[60vw] bg-pink-500 rounded-full blur-[100px] opacity-40 animate-blob animation-delay-4000" />
+      <div className="absolute bottom-[-10%] left-[20%] w-[70vw] h-[70vw] bg-pink-500 rounded-full blur-[100px] opacity-50 animate-blob animation-delay-4000" />
       
       {/* Center/Random - Blue */}
-      <div className="absolute top-[30%] left-[10%] w-[40vw] h-[40vw] bg-blue-600 rounded-full blur-[100px] opacity-30 animate-blob" />
+      <div className="absolute top-[30%] left-[10%] w-[50vw] h-[50vw] bg-blue-600 rounded-full blur-[100px] opacity-40 animate-blob" />
       
       {/* Noise overlay */}
       <div className="absolute inset-0 bg-[url('/noise.png')] opacity-[0.05] mix-blend-overlay"></div>
